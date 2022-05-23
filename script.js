ab9eed5515ee1713145132863d9f35d5b1d226eb
Yin <yinstanleycheung@gmail.com>
2022-05-23 15:20:05 -0700

    Let player pick r, p, or s

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Computer randomly chooses "rock", "paper", or "scissors"
 function computerPlay() {
-    const randInt = Math.floor(Math.random() * 3)
+    const randInt = Math.floor(Math.random() * 3);
     switch (randInt) {
         case 0:
             return "rock";
@@ -13,4 +13,13 @@ function computerPlay() {
     }
 }
 
+// Ask player to choose 
+function playerPlay() {
+    const playerChoice = prompt("Choose 'rock', 'paper', or 'scissors': ").trim().toLowerCase();
+    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+        return playerChoice;
+    } else {
+        return false;
+    }
+}
 
